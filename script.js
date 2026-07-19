150d1be108de46f3d0bbe06e036c85612fbdf5e0
pratikthorat16553-cpu <pratikthorat16553@gmail.com>
2026-07-20 01:09:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function searchTopic() {
+    let input = document.getElementById("searchBox").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        let text = cards[i].innerText.toLowerCase();
+
+        if (text.includes(input)) {
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+window.onload = function () {
+    console.log("Electric Wiring Master Loaded Successfully");
+};
